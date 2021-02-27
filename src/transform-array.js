27c16a2946ca2e80d120785961ca6b1e231562c6
AlexKuryanov <lexxus07@yandex.ru>
2021-02-28 00:56:38 +0300

    feat: begin to solve tasks

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!arr) {
+    throw `Error`
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === `--discard-next`) {
+        arr.splice(arr[i + 1], 1);
+      } else if (arr[i] === `--discard-prev`) {
+        arr.splice(arr[i - 1], 1);
+      } else if (arr[i] === `--double-next`) {
+        arr.splice(arr[i + 1], 0, arr[i + 1]);
+      } else if (arr[i] === `--double-prev`) {
+        arr.splice(arr[i - 1], 0, arr[i - 1]);
+      } else {
+        arr.splice(arr[i]);
+      }
+      
+    }
+  }
 };
