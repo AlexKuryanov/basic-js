f747e9648787450c41804a1547bb36abb7fe85de
AlexKuryanov <lexxus07@yandex.ru>
2021-03-29 23:22:47 +0300

    Revert "feat: extended-repeater"
    This reverts commit 4477a7167eadddd5cdf86ac5fb0adb36b0af2a01.

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,34 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  let defaultOptions = {
-    repeatTimes: 0,
-    separator: "+",
-    addition: "",
-    additionRepeatTimes: 0,
-    additionSeparator: "|",
-  };
-  let sep = options.separator;
-  //console.log(sep);
-  str = String(str);
-
-  if (sep) {
-    str = str + sep;
-  } else {
-    str = str + "+";
-  }
-
-  let newStr = str.repeat(options.repeatTimes);
-  //console.log(newStr)
-  let cep = new RegExp("" + sep + "$");
-  //console.log(cep)
-  let lastStr = "";
-  if (sep) {
-    lastStr = newStr.replace(cep, "");
-  } else {
-    lastStr = newStr.replace(/\+$/, "");
-  }
-
-  //console.log(lastStr);
-  return lastStr;
+module.exports = function repeater(/* str, options */) {
+  throw new CustomError('Not implemented');
+  // remove line with error and write your code here
 };
+  
