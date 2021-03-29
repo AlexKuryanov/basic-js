5a8a538eacf8da8bfe02937e6383f053bfe7f3e3
AlexKuryanov <lexxus07@yandex.ru>
2021-03-29 12:51:13 +0300

    feat: continue to solve tasks

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  
+  if (date === !new Date()) {
+    throw Error;
+  } else {
+    switch (date.getMonth()) {
+      case null:
+        return "Unable to determine the time of year!";
+        break;
+      case 11:
+      case 0:
+      case 1:
+        return "winter";
+        break;
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+        break;
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+        break;
+      case 8:
+      case 9:
+      case 10:
+        return "autumn | fall";
+        break;
+      default:
+        return "Unable to determine the time of year!";
+    }
+  }
 };
